7a90160f05fa7b7e59ceb4db2d660831e8edd51c
chrissileinus <chrissileinus@googlemail.com>
2021-10-28 08:38:39 +0000

    sdhgdg

--- src/Store.php
+++ src/Store.php
@@ -4,7 +4,7 @@ namespace Chrissileinus\Config;
 
 class Store implements \ArrayAccess, \Serializable, \JsonSerializable, \IteratorAggregate, \Traversable
 {
-  protected static $current = [];
+  protected static $storage = [];
 
   /**
    * set
@@ -23,7 +23,7 @@ class Store implements \ArrayAccess, \Serializable, \JsonSerializable, \Iterator
   {
     foreach ($args as $arg) {
       if (is_array($arg) || is_object($arg)) {
-        self::$current = array_replace_recursive(self::$current, (array) $arg);
+        self::importArray((array) $arg);
         continue;
       }
 
@@ -48,38 +48,55 @@ class Store implements \ArrayAccess, \Serializable, \JsonSerializable, \Iterator
     }
   }
 
+  /**
+   * importArray
+   *
+   * @param  mixed $array
+   * @return void
+   */
+  private static function importArray(array $array)
+  {
+    self::$storage = array_replace_recursive(self::$storage, $array);
+  }
+
+  /**
+   * importContent
+   *
+   * @param  mixed $content
+   * @return void
+   */
   private static function importContent(string $content)
   {
-    if ($value = \yaml_parse($content, 0, $_, [
+    if ($value = yaml_parse($content, 0, $_, [
       '!php' => function ($value, $tag, $flags) {
         [$class, $const] = explode('::', $value);
         return $class::getConstant($const);
       }
     ])) {
-      self::$current = array_replace_recursive(self::$current, (array) $value);
+      self::importArray((array) $value);
       return;
     }
 
     if ($value = json_decode($content, false, 512, JSON_OBJECT_AS_ARRAY)) {
-      self::$current = array_replace_recursive(self::$current, (array) $value);
+      self::importArray((array) $value);
       return;
     }
 
     if ($value = unserialize($content, false) && (is_array($value) || is_object($value))) {
-      self::$current = array_replace_recursive(self::$current, (array) $value);
+      self::importArray((array) $value);
       return;
     }
   }
 
   public static function get()
   {
-    return (object) self::$current;
+    return (object) self::$storage;
   }
 
   /** ArrayAccess */
   public function offsetExists($offset): bool
   {
-    return isset(self::$current[$offset]);
+    return isset(self::$storage[$offset]);
   }
 
   public function offsetSet($offset, $value)
@@ -88,7 +105,7 @@ class Store implements \ArrayAccess, \Serializable, \JsonSerializable, \Iterator
 
   public function offsetGet($offset): mixed
   {
-    return self::$current[$offset];
+    return self::$storage[$offset];
   }
 
   public function offsetUnset($offset)
@@ -98,23 +115,23 @@ class Store implements \ArrayAccess, \Serializable, \JsonSerializable, \Iterator
   /** Serializable  */
   public function serialize(): string
   {
-    return serialize(self::$current);
+    return serialize(self::$storage);
   }
 
   public function unserialize($data)
   {
-    self::$current = unserialize($data);
+    self::$storage = unserialize($data);
   }
 
   /** JsonSerializable */
   public function jsonSerialize()
   {
-    return self::$current;
+    return self::$storage;
   }
 
   /** */
   public function getIterator()
   {
-    return new \ArrayIterator(self::$current);
+    return new \ArrayIterator(self::$storage);
   }
 }
